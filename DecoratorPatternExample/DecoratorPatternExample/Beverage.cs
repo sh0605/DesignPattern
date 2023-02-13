a6454e7cca79122c8fae040decc09c1565cc208d
sh <ghkdthgus9@naver.com>
2023-02-13 14:46:17 +0900

    Decorator pattern example add

---
+++ DecoratorPatternExample/DecoratorPatternExample/Beverage.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPatternExample
+{
+    public abstract class Beverage
+    {
+        protected string description = "제목 없음";
+
+        public virtual string GetDescription()
+        { 
+            return description; 
+        }
+
+        public abstract double Cost();
+    }
+
+    public class Espresso : Beverage
+    {
+        public Espresso()
+        {
+            description = "에스프레소";
+        }
+
+        public override double Cost()
+        {
+            return 1.99;
+        }
+    }
+
+    public class HouseBlend : Beverage
+    {
+        public HouseBlend()
+        {
+            description = "하우스 블렌드 커피";
+        }
+
+        public override double Cost()
+        {
+            return 0.89;
+        }
+    }
+
+    public class DarkRoast : Beverage
+    {
+        public DarkRoast()
+        {
+            description = "다크 로스트 커피";
+        }
+
+        public override double Cost()
+        {
+            return 0.99;
+        }
+    }
+
+    public class Decaf : Beverage
+    {
+        public Decaf()
+        {
+            description = "디카페인 커피";
+        }
+
+        public override double Cost()
+        {
+            return 1.99;
+        }
+    }
+}
+
---
+++ DecoratorPatternExample/DecoratorPatternExample/CondimentDecorator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPatternExample
+{
+    public abstract class CondimentDecorator : Beverage
+    {
+        protected Beverage beverage;
+
+        public abstract override string GetDescription();
+    }
+
+    public class Mocha : CondimentDecorator
+    {
+        public Mocha(Beverage b)
+        {
+            beverage = b;
+        }
+        public override string GetDescription()
+        {
+            return beverage.GetDescription() + ", 모카";
+        }
+
+        public override double Cost()
+        {
+            return beverage.Cost() + 0.20;
+        }
+    }
+
+    public class Milk : CondimentDecorator
+    {
+        public Milk(Beverage b)
+        {
+            beverage = b;
+        }
+        public override string GetDescription()
+        {
+            return beverage.GetDescription() + ", 우유";
+        }
+
+        public override double Cost()
+        {
+            return beverage.Cost() + 0.10;
+        }
+    }
+    
+    public class Soy : CondimentDecorator
+    {
+        public Soy(Beverage b)
+        {
+            beverage = b;
+        }
+        public override string GetDescription()
+        {
+            return beverage.GetDescription() + ", 두유";
+        }
+
+        public override double Cost()
+        {
+            return beverage.Cost() + 0.15;
+        }
+    }
+
+    public class Whip : CondimentDecorator
+    {
+        public Whip(Beverage b)
+        {
+            beverage = b;
+        }
+        public override string GetDescription()
+        {
+            return beverage.GetDescription() + ", 휘핑크림";
+        }
+
+        public override double Cost()
+        {
+            return beverage.Cost() + 0.10;
+        }
+    }
+}
